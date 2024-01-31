1087857f7ae1b1b72d2db45cd82ab32ea48f1ce2
sayak <sayakchowdhury384@gmail.com>
2024-02-01 04:25:30 +0530

    assignment 1b completed

--- opencv_practice/assignment1b.py
+++ opencv_practice/assignment1b.py
@@ -1,33 +1,43 @@
+# importing required libraries
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
+#'''
+image = cv.imread('ShadowRemoval2.jpg')
+gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
-img = cv.imread('ShadowRemoval1.jpg')
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-ret, thresh = cv.threshold(gray, 60, 200, cv.THRESH_BINARY)
+ret, thresh = cv.threshold(gray, 110, 200, cv.THRESH_BINARY)
 blur = cv.GaussianBlur(thresh, (7, 7), cv.BORDER_DEFAULT)
 canny = cv.Canny(blur, 100, 175)
-dilated = cv.dilate(canny, (1, 1), iterations=0)
+dilated = cv.dilate(canny, (1, 1), iterations=1)
+
 (cnt, hierarchy) = cv.findContours(dilated.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE) 
-rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB) 
+rgb = cv.cvtColor(image, cv.COLOR_BGR2RGB) 
 cv.drawContours(rgb, cnt, -1, (255, 0, 0), 2) 
 
 plt.imshow(rgb)
 plt.show()
 
+
 '''
-image = cv2.imread('ShadowRemoval1.jpg') 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
+# loading .jpg image using cv.imread() function into 'image'
+image = cv.imread('ShadowRemoval2.jpg') 
+# converting the RGB image in grayscale
+# converting from a 3-channel image matrix to a 2-D matrix
+gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY) 
 
-blur = cv2.GaussianBlur(gray, (11, 11), 0) 
-canny = cv2.Canny(blur, 30, 150, 3) 
-dilated = cv2.dilate(canny, (1, 1), iterations=0) 
+# applying a Gaussian filter (kernel dimensions of (11, 11))
+blur = cv.GaussianBlur(gray, (11, 11), 0) 
+# applying cv.Canny() for edge detection
+canny = cv.Canny(blur, 30, 150, 3)
+# dilating the image for more distinct edges
+dilated = cv.dilate(canny, (3, 3), iterations=1) 
 
-(cnt, hierarchy) = cv2.findContours( 
-	dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
-rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) 
-cv2.drawContours(rgb, cnt, -1, (0, 255, 0), 2) 
+
+(cnt, hierarchy) = cv.findContours(dilated.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE) 
+rgb = cv.cvtColor(image, cv.COLOR_BGR2RGB) 
+cv.drawContours(rgb, cnt, -1, (255, 0, 0), 2) 
 
 plt.imshow(rgb)
 plt.show()
